241b4d824bdfaaa771ff7bc199e4fb42a4b45651
Rudraksh MK <contact@rudimk.online>
2018-11-06 17:01:42 +0530

    Moving the library module into the engine module.

--- library/agents.py
+++
@@ -1,8 +0,0 @@
-import requests as r
-
-def getAgent():
-    return r.get('http://pplapi.com/random.json').json()
-
-def getAgentByCountry(country):
-    url = "http://pplapi.com/country/" + country + "/random.json"
-    return r.get(url).json()
--- library/clock.py
+++
@@ -1,7 +0,0 @@
-import arrow
-
-def getCurrentTime():
-    return arrow.now().to('Asia/Kolkata').format('DD-MMM-YYYY HH:mm:ss ZZ')
-
-def getZoneTime(tz):
-    return arrow.now().to(tz).format('DD-MMM-YYYY HH:mm:ss ZZ')
